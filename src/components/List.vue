b32231c394d2e21165dc097688314178107904aa
askaper <andrew@skaper.info>
2016-11-28 21:16:24 -0600

    Modified scaffold to fit application requirements

---
+++ src/components/List.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+  <div class="test-1">
+    <h1 class="heading">{{ msg }}</h1>
+    <div class="input-box">
+      <button></button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'list',
+  data () {
+    return {
+      msg: 'Testing!'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.heading {
+  color: pink;
+}
+
+</style>
